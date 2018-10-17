fff3234ffb19bc57888730167fe81f17d0815cf5
Baizey <lassemoerk.mrk@gmail.com>
2018-10-17 21:34:49 +0200

    fix missing checks for $define command

--- bin/commands/Define.js
+++ bin/commands/Define.js
@@ -15,6 +15,8 @@ module.exports = new Command(
         const url = `api.urbandictionary.com/v0/define?term=${message.msg.trim()}`;
 
         Api.request(url).then(resp => {
+            if (utils.isUndefined(resp.result) || utils.isUndefined(resp.result.list))
+                return bot.sendMsg("No definition exists... probably", message);
             if (resp.isFailure || resp.result.list.length === 0)
                 return bot.sendMsg("No definition exists... probably", message);
             return bot.sendMsg(resp.result.list[0].definition.replace(/[\[\]]/g, ''), message);
