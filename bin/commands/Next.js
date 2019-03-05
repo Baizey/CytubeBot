f89c4f9b7d63dbfe3261eeadd1d56fb8e356a3d2
Baizey <Baizey@users.noreply.github.com>
2019-03-05 12:26:46 +0100

    moved next to mod command

--- bin/commands/Next.js
+++ bin/commands/Next.js
@@ -3,18 +3,18 @@ const Command = require("../structure/Command");
 const utils = require("../core/Utils");
 
 module.exports = new Command(
-    rank.guest,
+    rank.mod,
     "",
     (bot, message) => {
         const data = bot.playlist.dataTillNextMovie;
 
         // No intermissions
         if (utils.isEmpty(data.intermissions))
-            return bot.sendMsg(`Next movie in the queue is ${data.next.displayTitle}, a poll or moderator may change this`, message);
+            return bot.sendMsg(`Next movie in the queue is ${data.next.displayTitle}`, message);
 
         bot.sendMsg([
             `${data.time.asUnit} before next movie (${data.intermissions.length} intermissions)`,
-            `Next movie in the queue is ${data.next.displayTitle}, a poll or moderator may change this`
+            `Next movie in the queue is ${data.next.displayTitle}`
         ], message);
     }
 );
