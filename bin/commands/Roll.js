5c4bbea92e85d2a55b3aa0bd468f62432d044c1b
Baizey <Baizey@users.noreply.github.com>
2019-03-11 17:56:22 +0100

    better text reply for rolls

--- bin/commands/Roll.js
+++ bin/commands/Roll.js
@@ -1,14 +1,12 @@
 const rank = require("../structure/Ranks");
 const Command = require("../structure/Command");
 const utils = require("../core/Utils");
-const Api = require("../core/Api");
-const Video = require("../structure/Playlist").Video;
 
 module.exports = new Command(
     rank.guest,
     "",
     (bot, message) => {
-        const dices = message.msg.split(' ')
+        const dices = message.msg.split(';')
             .filter(d => /^\d+d\d+$/.test(d))
             .map(d => new Dice(d));
         bot.sendMsg(dices.map(d => d.roll()), message);
@@ -21,14 +19,21 @@ class Dice {
         this.text = text;
         const t = text.split('d');
         this.amount = Math.min(10, Number(t[0]));
-        this.sides = Math.min(9999, Number(t[1]));
+        this.sides = Math.max(2, Math.min(9999, Number(t[1])));
     }
 
     roll() {
         const result = [];
-        for (let i = 0; i < this.amount; i++)
-            result.push(utils.random(1, this.sides + 1));
-        return this.text + ': ' + result.join(', ');
+        for (let i = 0; i < this.amount; i++) {
+            const number = utils.random(1, this.sides + 1);
+            if (number === this.sides)
+                result.push(`Natural ${number}!`);
+            else if (number === 1)
+                result.push(`Critical Failure`);
+            else
+                result.push(utils.random(1, this.sides + 1));
+        }
+        return `Rolling ${this.amount} d${this.sides}... ${result.join(', ')}`;
     }
 
 }
