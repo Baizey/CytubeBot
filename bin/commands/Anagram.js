35725151ae62a93f40e77bffd15382c4fec1847d
Baizey <Baizey@users.noreply.github.com>
2019-03-12 23:35:24 +0100

    fixed issue with over 500 anagram results

--- bin/commands/Anagram.js
+++ bin/commands/Anagram.js
@@ -22,13 +22,22 @@ module.exports = new Command(
             logger.debug(resp.result);
 
             // TODO: remove hack, Hack to get anagram results as the api doesnt have an actual api :(
-            const anagrams = resp.result.split('Displaying all:')[1].split('<script>')[0].replace(/<br>|<\/b>/g, '').trim().split('\n')
+            const anagrams = getAnagrams(resp.result);
 
             logger.debug(anagrams);
 
-            if (anagrams.length > 0)
+            if (utils.isUndefined(anagrams))
+                bot.sendMsg('Something went wrong getting anagrams :(', message);
+            else if (anagrams.length > 0)
                 bot.sendMsg(`Anagram: ${utils.random(anagrams)}`, message);
             else
                 bot.sendMsg(`Found no anagrams for ${message.msg}`, message);
         })
 );
+
+function getAnagrams(body) {
+    let temp = body.split('Displaying all:')[1];
+    if (!temp) temp = body.split('Displaying first 500:')[1];
+    if (!temp) return null;
+    return temp.split('<script>')[0].replace(/<br>|<\/b>/g, '').trim().split('\n');
+}
